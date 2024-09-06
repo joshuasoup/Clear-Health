7c52809cc48bee80308825fdedcb2eaf527ea60b
joshuasoup <109001280+joshuasoup@users.noreply.github.com>
2024-09-05 20:46:15 -0400

    database error

--- src/lib/mongo/db.jsx
+++ src/lib/mongo/db.jsx
@@ -8,22 +8,37 @@ let clientPromise;
 
 // Ensure MongoDB URI is set
 if (!process.env.MONGODB_URL) {
-  throw new Error("Please add your Mongo URI to .env.local");
+  throw new Error("Please add your Mongo URI to the environment variables");
 }
 
-if (process.env.NODE_ENV == "development") {
+if (process.env.NODE_ENV === "development") {
   if (!global._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    global._mongoClientPromise = client
+      .connect()
+      .then(() => {
+        console.log("Successfully connected to MongoDB in development mode");
+        return client;
+      })
+      .catch((err) => {
+        console.error("Failed to connect to MongoDB in development", err);
+        throw err;
+      });
   }
   clientPromise = global._mongoClientPromise;
 } else {
   // In production mode, create a new connection instance
   client = new MongoClient(uri, options);
-  clientPromise = client.connect().catch((err) => {
-    console.error("Failed to connect to MongoDB", err);
-    throw err; // Ensure any connection issues throw an error in production
-  });
+  clientPromise = client
+    .connect()
+    .then(() => {
+      console.log("Successfully connected to MongoDB in production mode");
+      return client;
+    })
+    .catch((err) => {
+      console.error("Failed to connect to MongoDB in production", err);
+      throw err; // Ensure connection issues throw an error in production
+    });
 }
 
 export default clientPromise;
