c12fdb3e28b3f274597b9688a630ff01efd10822
joshuasoup <109001280+joshuasoup@users.noreply.github.com>
2024-09-22 12:35:39 -0400

    now it works

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)' , '/', '/about', '/contact', '/privacy', '/terms', '/pricing' , '/roadmap', '/api/webhooks/(.*), /assets/(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)' , '/', '/about', '/contact', '/privacy', '/terms', '/pricing' , '/roadmap', '/api/webhooks/(.*)', '/assets/(.*)'])
 
 export default clerkMiddleware((auth, request) => {
   if (!isPublicRoute(request)) {
